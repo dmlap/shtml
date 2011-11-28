01c5a7c0c22d07e7bfc7e9eee26f982c0f823910
David LaPalomento <dlapalomento@gmail.com>
2011-11-27 18:37:28 -0600

    parse text nodes. parse attributes. refactor out common functionality

--- src/main/scala/HtmlInterpreter.scala
+++ src/main/scala/HtmlInterpreter.scala
@@ -1,9 +1,11 @@
 package com.github.dmlap.shtml
 
+import scala.collection.JavaConverters._
+
 sealed trait HtmlEvent
 object StartDoc extends HtmlEvent
 object EndDoc extends HtmlEvent
-final case class StartElem(name: String) extends HtmlEvent
+final case class StartElem(name: String, attributes: Map[String, String]) extends HtmlEvent
 object EndElem extends HtmlEvent
 final case class TextNode(text: String) extends HtmlEvent
 
@@ -14,63 +16,101 @@ trait HtmlTokenizer {
 object HtmlInterpreter extends Application {
   implicit object XmlTokenizer extends HtmlTokenizer {
     import javax.xml.stream.XMLInputFactory
-    import javax.xml.stream.events.{StartDocument, StartElement, EndDocument, EndElement, Characters}
+    import javax.xml.stream.events.{Attribute, StartDocument, StartElement, EndDocument, EndElement, Characters}
     import java.io.StringReader
+
     def apply(html: String): Iterator[HtmlEvent] = new Iterator[HtmlEvent] {
       val tokens = XMLInputFactory.newInstance().createXMLEventReader(new StringReader(html))
-      def hasNext() = tokens.hasNext()
+      def hasNext = tokens.hasNext
       def next() = tokens.next() match {
         case _: StartDocument => StartDoc
         case _: EndDocument => EndDoc
-        case startElem: StartElement => StartElem(startElem.getName.getLocalPart)
+        case startElem: StartElement => {
+          val attrs = (Map.empty[String, String] /: startElem.getAttributes.asScala) {
+            case (attrs: Map[String, String], attr: Attribute) =>
+            attrs + (attr.getName.getLocalPart -> attr.getValue)
+          }
+          StartElem(startElem.getName.getLocalPart, attrs)
+        }
         case _: EndElement => EndElem
         case text: Characters => TextNode(text.getData)
       }
     }
   }
 
-  def parse(html: String)(implicit tokenizer: HtmlTokenizer): String = {
+  @inline private def indent(level: Int, builder: StringBuilder) {
+    (0 until level) foreach { _ => builder.append("  ") }
+  }
+  @inline private def obj(seq: Int, builder: StringBuilder) {
+    builder.append("object _")
+    builder.append(seq)
+  }
+  @inline private def prop(name: String, value: String, builder: StringBuilder) {
+    builder.append("val " + name + " = \"")
+    builder.append(value)
+    builder.append("\"\n")
+  }
+  @inline private def elemStart(className: String,
+                                name: String,
+                                attrs: Map[String, String],
+                                level: Int,
+                                builder: StringBuilder) {
+    builder.append("object ")
+    builder.append(className)
+    builder.append(" implements Elem {\n")
+    indent(level + 1, builder)
+    prop("name", name, builder)
+    attrs foreach { attr =>
+      indent(level + 1, builder)
+      prop("`" + attr._1 + "`", attr._2, builder)
+    }
+  }
+
+  def parse(pkg: List[String],
+            className: String,
+            html: String)(implicit tokenizer: HtmlTokenizer): String = {
     val result = new StringBuilder()
     val itr = tokenizer(html)
     var seq = List(0)
     var level = 0
+    var foundRoot = false
+
+    result.append("package ")
+    result.append(pkg.mkString("."))
+    result.append("\n")
+
+    while (!foundRoot && itr.hasNext) {
+      itr.next match {
+        case StartElem(name, attrs) => {
+          elemStart(className, name, attrs, 0, result)
+          level += 1
+          seq = 0 :: (seq.head + 1) :: seq.tail
+          foundRoot = true
+        }
+        case _ => ()
+      }
+    }
     while (itr.hasNext) {
       itr.next match {
-        case StartElem(name) => {
-          val indent = new StringBuilder()
-          (0 until level) foreach { _ => indent.append("  ") }
-          result.append(indent)
-          result.append("object _")
-          result.append(seq.head)
-          result.append(" implements Elem {\n")
-          result.append(indent)
-          result.append("  ")
-          result.append("val name = \"")
-          result.append(name)
-          result.append("\"\n")
+        case StartElem(name, attrs) => {
+          indent(level, result)
+          elemStart("_" + seq.head, name, attrs, level, result)
           level += 1
           seq = 0 :: (seq.head + 1) :: seq.tail
         }
         case `EndElem` => {
-          val indent = new StringBuilder()
-          (0 until level - 1) foreach { _ => indent.append("  ") }
-          result.append(indent)
+          indent(level - 1, result)
           level -= 1
           seq = seq.tail
           result.append("}\n")
         }
         case TextNode(text) => {
-          val indent = new StringBuilder()
-          (0 until level) foreach { _ => indent.append("  ") }
-          result.append(indent)
-          result.append("object _")
-          result.append(seq.head)
+          indent(level, result)
+          obj(seq.head, result)
           result.append(" implements Text {\n")
-          result.append(indent)
-          result.append("  val text = \"")
-          result.append(text)
-          result.append("\"\n")
-          result.append(indent)
+          indent(level + 1, result)
+          prop("text", text, result)
+          indent(level, result)
           result.append("}\n")
           seq = (seq.head + 1) :: seq.tail
         }
@@ -78,43 +118,5 @@ object HtmlInterpreter extends Application {
       }
     }
     result.toString
-  }
-  
-  // testing
-  case class ProductMatcher(product: Product) {
-    def matches(rhs: Map[String, Any]) = false
-  }
-
-  def wrap(body: String): String = 
-    "<html><head><title></title></head><body>" +
-      body +
-      "</body></html>"
-
-  override def main(args: Array[String]) {
-    println("Parse should:")
-    println("correctly interpret a valid, minimal HTML document:")
-    val actualTrivial = parse(wrap(""))
-    val expectedTrivial =
-      "object _0 implements Elem {\n" +
-      "  val name = \"html\"\n" +
-      "  object _0 implements Elem {\n" +
-      "    val name = \"head\"\n" +
-      "    object _0 implements Elem {\n" +
-      "      val name = \"title\"\n" +
-      "    }\n" +
-      "  }\n" +
-      "  object _1 implements Elem {\n" +
-      "    val name = \"body\"\n" +
-      "  }\n" +
-      "}\n"
-    println("\t" + (actualTrivial == expectedTrivial))
-
-    println("create properties for child elements of the body")
-    val actualPs = parse(wrap("<p /><p /><p />"))
-    val hasPs = java.util.regex.Pattern.compile(".*(.+val name = \"p\"){3}.*", java.util.regex.Pattern.DOTALL)
-    println("\t" + (hasPs.matcher(actualPs).matches))
-    println("creates text nodes")
-    val text = parse(wrap("abc"))
-    println("\t" + (text.indexOf("val text = \"abc\"") >= 0))
   }
 }
--- src/test/scala/HtmlInterpreterSpecs.scala
+++ src/test/scala/HtmlInterpreterSpecs.scala
@@ -1,7 +1,44 @@
 package com.github.dmlap.shtml
 
-object HtmlInterpreterSpecs extends Application {
-  override def main(args: Array[String]) {
-    println("hi")
+import org.specs2.mutable._
+import HtmlInterpreter.XmlTokenizer
+
+object HtmlInterpreterSpecs extends Specification {
+  "HtmlInterpreter" should {
+    "correctly interpret a valid, minimal HTML document:" in {
+      val actualTrivial = parseHtmlWithBody("")
+      val expectedTrivial =
+      "package com.example\n" +  
+      "object Example implements Elem {\n" +
+      "  val name = \"html\"\n" +
+      "  object _0 implements Elem {\n" +
+      "    val name = \"head\"\n" +
+      "    object _0 implements Elem {\n" +
+      "      val name = \"title\"\n" +
+      "    }\n" +
+      "  }\n" +
+      "  object _1 implements Elem {\n" +
+      "    val name = \"body\"\n" +
+      "  }\n" +
+      "}\n"
+      actualTrivial must_== expectedTrivial
+    }
+    "create properties for child elements of the body" in {
+      val actualPs = parseHtmlWithBody("<p /><p /><p />")
+      val hasPs = java.util.regex.Pattern.compile(".*(.+val name = \"p\"){3}.*", java.util.regex.Pattern.DOTALL)
+      true must_== hasPs.matcher(actualPs).matches
+    }
+    "create text nodes" in {
+      true must_== (parseHtmlWithBody("abc").indexOf("val text = \"abc\"") >= 0)
+    }
+    "record attributes" in {
+      true must_== (parseHtmlWithBody("<div class='c0 c1' />").indexOf("val `class` = \"c0 c1\"") >= 0)
+    }
   }
+  def parseHtmlWithBody(body: String): String = 
+    HtmlInterpreter.parse("com" :: "example" :: Nil,
+                          "Example",
+                          "<html><head><title></title></head><body>" +
+                          body +
+                          "</body></html>")
 }
